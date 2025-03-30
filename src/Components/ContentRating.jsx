e5cc5f5dc3f95dbd1120415dbbbb799f55db345d
KINGAKWO <kingakwomakembe@gmail.com>
2025-03-30 12:56:06 +0100

    created a component for content rating

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -5,14 +5,47 @@ import './ContentRating.css';
 class ContentRating extends Component {
   constructor() {
     super();
+    this.state = {
+      likeCount: 0,
+      dislikeCount: 0,
+      totalRatings: 0,
+      handleLike:() => {
+        this.setState((prevState) => ({
+          likeCount: prevState.likeCount + 1,
+          totalRatings: prevState.totalRatings + 1,
+        }));
+        
+      },
+      handleDislike:() => {
+        this.setState((prevState) => ({
+          dislikeCount: prevState.dislikeCount + 1,
+          totalRatings: prevState.totalRatings + 1,
+        }));
+      },
+    };
+    
   }
   render() {
     return (
      <>
-     <h1>Text Content Rating</h1>
+     <div className="content-rating">
+      <p>
+        Solo leveling is peak 
+      </p>
+     </div>
+     <div className='rating-buttons'>
+      <button className="like-buttons" onClick={this.state.handleLike}>
+        Like({this.state.likeCount})
+      </button>
+      <button className="dislike-buttons" onClick={this.state.handleDislike}>
+        Dislike({this.state.dislikeCount})
+      </button>
+     </div>
+    <p className="total-ratings">Total Ratings: {this.state.totalRatings}</p> 
      </>
     );
   }
 }
+
 
 export default ContentRating;
